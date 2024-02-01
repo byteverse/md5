0f3140358d39d99784b41e86ab1658facf7a02f7
Brian McKeon <bmckeon@layer3com.com>
2024-02-01 10:58:03 -0500

    Prepare 0.1.0.3 release. Reformatted. Added workflows. Updated package metadata.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Revision history for sha1
 
+## 0.1.0.3 -- 2024-02-01
+
+* Update package metadata.
+
 ## 0.1.0.2 -- 2020-03-09
 
 * Add proper attribution to license file.
--- src/Hash/Sha1.hs
+++ src/Hash/Sha1.hs
@@ -1,6 +1,6 @@
-{-# language DataKinds #-}
-{-# language MagicHash #-}
-{-# language UnliftedFFITypes #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE MagicHash #-}
+{-# LANGUAGE UnliftedFFITypes #-}
 
 module Hash.Sha1
   ( boundedBuilder
@@ -9,9 +9,9 @@ module Hash.Sha1
 import Control.Monad.ST (ST)
 import Data.Bytes.Builder.Bounded as BB
 import Data.Bytes.Builder.Bounded.Unsafe as BBU
-import Data.Bytes.Types (Bytes(Bytes))
-import Data.Primitive (ByteArray(..),MutableByteArray(..))
-import GHC.Exts (Int(I#),Int#,MutableByteArray#,ByteArray#)
+import Data.Bytes.Types (Bytes (Bytes))
+import Data.Primitive (ByteArray (..), MutableByteArray (..))
+import GHC.Exts (ByteArray#, Int (I#), Int#, MutableByteArray#)
 import GHC.IO (unsafeIOToST)
 
 foreign import ccall unsafe "sha1.h hs_cryptohash_sha1_onepass"
@@ -22,8 +22,9 @@ performHash (MutableByteArray x) (I# a) (ByteArray y) (I# b) (I# c) =
   unsafeIOToST (c_hash x a y b c)
 
 boundedBuilder :: Bytes -> BB.Builder 20
-boundedBuilder (Bytes arr off len) = BBU.construct
-  (\buf ix -> do
-    performHash buf ix arr off len
-    pure (ix + 20)
-  )
+boundedBuilder (Bytes arr off len) =
+  BBU.construct
+    ( \buf ix -> do
+        performHash buf ix arr off len
+        pure (ix + 20)
+    )
